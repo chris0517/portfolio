525ddf76374a7bf321c0f64ca758592ce24350d1
Chris Wang <chris92563@outlook.com>
2024-01-10 00:35:27 -0500

    commit

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './App.css';
-import profile from './assets/photo.png';
+import profile from './assets/pfp.jpg';
 import project from './assets/project.png';
 import {Grid, Button, FormControl, InputLabel, Select, MenuItem, Typography, TextField} from '@mui/material';
 
@@ -44,14 +44,14 @@ function App() {
             </div>
             <div></div>
             <div>
-              <img src={profile} width={220}/>
+              <img src={profile} className="rounded-full" width={300}/>
             </div>
           </div>
         </section>
 
         {/* Projects */}
         <section>
-          <div className='container m-auto px-4'>
+          <div className='container m-auto px-4 py-12'>
             <h2 className='text-2xl font-semiibold'>Projects</h2>
             <div className='flex flex-wrap gap-10 mt-11'>
               <div className='border rounded-md p-3'>
@@ -96,6 +96,105 @@ function App() {
               </div>
             </div>
           </div>
+        </section>
+
+        {/* Tech */}
+        <section className="py-10">
+        <div className='container m-auto px-4'>
+            <h2 className='text-2xl font-semiibold'>Languages</h2>
+            <div className="mt-4">
+              <div>
+                <div className="flex justify-between items-center">
+                  <h2 className='font-semibold'>Java</h2>
+                  <p className="text-gray-500 text-righ">Advanced</p>
+                </div>
+                <span className="w-full h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md" />
+              </div>
+
+              <div className='mt-6'>
+                <div className="flex justify-between items-center">
+                  <h2 className='font-semibold'>Javascript</h2>
+                  <p className="text-gray-500">Advanced</p>
+                </div>
+                <span className="w-full h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md" />
+              </div>
+
+              <div className='mt-6'>
+                <div className="flex justify-between items-center">
+                  <h2 className='font-semibold'>Python</h2>
+                  <p className="text-gray-500">Advanced</p>
+                </div>
+                <span className="w-full h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md" />
+              </div>
+
+              <div className='mt-6'>
+                <div className="flex justify-between items-center">
+                  <h2 className='font-semibold'>C#</h2>
+                  <p className="text-gray-500">Intermediate</p>
+                </div>
+                <span className="w-[75%] h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md" />
+              </div>
+            </div>
+        
+        
+        </div>
+        </section>
+
+        {/* Additional Skills */}
+        <section>
+          <div className='container m-auto px-4 py-14'>
+            <h2 className='text-2xl font-semiibold'>Technologies</h2>
+            <div className='flex justify-between mt-6 w-[80%]'>
+              <div>
+                <p className="font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-5">
+                  Git
+                </p>
+              </div>
+              <div>
+                <p className='font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-5'>
+                  Git
+                </p>              
+              </div>
+              <div>
+                <p className='font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-5'>
+                  Git
+                </p>
+              </div>
+              <div>
+                <p className='font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-5'>
+                  Git
+                </p>
+              </div>
+            </div>
+
+            <div className='flex justify-between mt-6 w-[80%]'>
+              <div>
+                <p className="font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-5">
+                  Git
+                </p>
+              </div>
+              <div>
+                <p className='font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-5'>
+                  Git
+                </p>              
+              </div>
+              <div>
+                <p className='font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-5'>
+                  Git
+                </p>
+              </div>
+              <div>
+                <p className='font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-5'>
+                  Git
+                </p>
+              </div>
+            </div>
+          </div>
+
+
+        </section>
+          <div></div>
+        <section>
 
         </section>
       </main>
